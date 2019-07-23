77ca1becb2d44b783ef20ee4b630ecb32b818b89
Tiago-PHP <tiagojonatassilva32@gmail.com>
2019-07-23 16:45:42 -0300

    teste alteração

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                 Digital Brasilia
                 </div>
 
                 <div class="links">
