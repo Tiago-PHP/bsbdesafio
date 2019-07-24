3764fa966c24278a70b5e24a867d2f40a99e4914
Tiago-PHP <tiagojonatassilva32@gmail.com>
2019-07-24 14:49:49 -0300

    Produtos (update-migrate)

--- database/seeds/ProdutosTableSeeder.php
+++ database/seeds/ProdutosTableSeeder.php
@@ -21,7 +21,46 @@ class ProdutosTableSeeder extends Seeder
                 "created_at"        => $now,
                 "updated_at"        => $now,
             ],
-           
+            [
+                'id'                => 2,
+                'nome'              => 'Notebook',
+                'valor_unitario'    => 2000.0,
+                'quantidade'        => 5,
+                "created_at"        => $now,
+                "updated_at"        => $now,
+            ],
+            [
+                'id'                => 3,
+                'nome'              => 'Tablet SAmsung',
+                'valor_unitario'    => 600.0,
+                'quantidade'        => 12,
+                "created_at"        => $now,
+                "updated_at"        => $now,
+            ],
+            [
+                'id'                => 4,
+                'nome'              => 'Motorola MOTO G7',
+                'valor_unitario'    => 1180.0,
+                'quantidade'        => 5,
+                "created_at"        => $now,
+                "updated_at"        => $now,
+            ],
+            [
+                'id'                => 5,
+                'nome'              => 'Zen fone Asus',
+                'valor_unitario'    => 1230.0,
+                'quantidade'        => 7,
+                "created_at"        => $now,
+                "updated_at"        => $now,
+            ],
+            [
+                'id'                => 6,
+                'nome'              => 'All in One-LG',
+                'valor_unitario'    => 2230.0,
+                'quantidade'        => 7,
+                "created_at"        => $now,
+                "updated_at"        => $now,
+            ]        
         ]);
     }
 }
